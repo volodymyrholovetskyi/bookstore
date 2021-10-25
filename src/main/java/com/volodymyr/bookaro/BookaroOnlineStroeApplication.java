196d709d8aa948eae5563c3ffcee4c6fa5d9435e
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2021-10-25 17:54:25 +0200

    add an empty project

---
+++ src/main/java/com/volodymyr/bookaro/BookaroOnlineStroeApplication.java
@@ -0,0 +1,13 @@
+package com.volodymyr.bookaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookaroOnlineStroeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookaroOnlineStroeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volodymyr/bookaro/BookaroOnlineStroeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volodymyr.bookaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookaroOnlineStroeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
